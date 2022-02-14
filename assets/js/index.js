08caef7251cdf1dd7ffafe748c97403e65e00c4f
Amircharkhchi <amircharkhchi53@gmail.com>
2022-02-14 19:49:10 +0330

    side menu created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,21 @@
 //Variable
 
 const emailInput = document.querySelector('#email-input');
+const navbar = document.querySelector("#Sidenav");
+const navbarOutSide = document.querySelector("#sidebar-overlay");
+
 
 //Event Listener
 
 (function () {
     emailInput.addEventListener('blur' , validateEmail)	 
 })();
+
+eventListener();
+function eventListener() {
+  document.querySelector("#hamburgerMenu").addEventListener("click", openNav);
+  navbarOutSide.addEventListener("click", closeNav);
+}
 
 //functions
 
@@ -18,3 +27,38 @@ function validateEmail(){
         this.parentElement.style.borderColor = "red";  
     }
 }
+
+function openNav() {
+    navbar.style.width = "300px";
+    document.querySelectorAll("#Sidenav a").forEach((e) => {
+      e.style.display = "block";
+    });
+    navbarOutSide.style.display = "block";
+  }
+  
+  function closeNav() {
+    navbar.style.width = "0";
+    document.querySelectorAll("#Sidenav a").forEach((e) => {
+      e.style.display = "none";
+    });
+    navbarOutSide.style.display = "none";
+  }
+
+//loop
+if (document.getElementById("Sidenav")) {
+    document.querySelectorAll("#Sidenav a").forEach((e) => {
+      e.style.display = "none";
+    });
+  }
+
+
+let createRedBox = () => {
+  let mainBox = document.querySelector("#contact-us-box");
+  let redBox = document.querySelector("#box-red");
+  redBox.style.width = `${mainBox.offsetWidth}px`;
+  redBox.style.height = `${mainBox.offsetHeight}px`;
+};
+createRedBox();
+
+
+
